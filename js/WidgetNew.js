3fe26cdf866e2d6e951fdf6bb37591db4eade69b
ivanrnld <ivan.reinaldo92@gmail.com>
2013-09-11 13:19:12 +0800

    Delete conflicting libraries

--- js/WidgetNew.js
+++
@@ -1,9 +0,0 @@
-var mainSvg = d3.select("#viz")
-                .append("svg")
-                .attr("width", MAIN_SVG_WIDTH)
-                .attr("height", MAIN_SVG_HEIGHT);
-
-var pseudocodeSvg = d3.select("#pseudocode")
-                      .append("svg")
-                      .attr("width", PSEUDOCODE_SVG_WIDTH)
-                      .attr("height", PSEUDOCODE_SVG_HEIGHT);
