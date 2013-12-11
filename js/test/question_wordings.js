a43a39ee91bbde684e14a8236f1e79f8d72a679c
Ivan Reinaldo <ivan.reinaldo92@gmail.com>
2013-12-11 12:48:25 +0800

    Added question wordings for min and max value of BST

--- js/test/question_wordings.js
+++ js/test/question_wordings.js
@@ -2,3 +2,5 @@ const BST_SEARCH = "Given the BST as shown in the picture, click the sequence of
 const BST_TRAVERSAL = "Given the BST as shown in the picture, click the sequence of vertices (the order matters) that are visited by |subtype| traversal!";
 const BST_SUCCESSOR = "Given the BST as shown in the picture, click the sequence of vertices (the order matters) that are visited by Successor(|value|)!";
 const BST_PREDECESSOR = "Given the BST as shown in the picture, click the sequence of vertices (the order matters) that are visited by Predecessor(|value|)!";
+const BST_MIN = "What is the value of the minimum element in this BST?";
+const BST_MAX = "What is the value of the minimum element in this BST?";
